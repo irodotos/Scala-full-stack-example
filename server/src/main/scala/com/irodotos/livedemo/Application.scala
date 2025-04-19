57ef3b7bd01766d645460d91f3af175d11912d1a
irodotos <heroapostolou@gmail.com>
2025-04-19 17:38:41 +0300

    server is running

---
+++ server/src/main/scala/com/irodotos/livedemo/Application.scala
@@ -0,0 +1,39 @@
+package com.irodotos.livedemo
+
+import cats.effect.*
+import doobie.util.ExecutionContexts
+import doobie.hikari.HikariTransactor
+import com.comcast.ip4s.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.middleware.CORS
+
+import com.irodotos.livedemo.core.*
+import com.irodotos.livedemo.http.*
+
+object Application extends IOApp.Simple {
+  def makePostgres = for {
+    ec <- ExecutionContexts.fixedThreadPool[IO](32)
+    transactor <- HikariTransactor.newHikariTransactor[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql://localhost:5444/",
+      "docker",
+      "docker",
+      ec
+    )
+  } yield transactor
+
+  def makeServer = for {
+    postgres <- makePostgres
+    jobs     <- JobsLive.resource[IO](postgres)
+    jobApi   <- JobRoutes.resource[IO](jobs)
+    server <- EmberServerBuilder
+      .default[IO]
+      .withHost(host"0.0.0.0")
+      .withPort(port"4041")
+      .withHttpApp(CORS(jobApi.routes.orNotFound))
+      .build
+  } yield server
+
+  override def run: IO[Unit] =
+    makeServer.use(_ => IO.println("Server is running at 0.0.0.0:4041") *> IO.never)
+}
--- server/src/main/scala/com/irodotos/livedemo/http/JobRoutes.scala
+++ server/src/main/scala/com/irodotos/livedemo/http/JobRoutes.scala
@@ -1,5 +1,41 @@
 package com.irodotos.livedemo.http
 
-class JobRoutes {
+import cats.effect.*
+import cats.*
+import cats.syntax.all.*
+import org.http4s.*
+import org.http4s.implicits.*
+import org.http4s.dsl.Http4sDsl
+import com.irodotos.livedemo.core.*
+import com.irodotos.livedemo.domain.job.Job
+import org.http4s.server.Router
+import org.http4s.circe.CirceEntityCodec.*
+import io.circe.generic.auto.*
 
+class JobRoutes[F[_]: Concurrent] private (jobs: Jobs[F]) extends Http4sDsl[F] {
+  private val prefix = "/jobs"
+
+  // post /jobs/create { Job }
+  private val createJobRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case req@POST -> Root / "create" =>
+      for {
+        job <- req.as[Job]
+        id <- jobs.create(job)
+        resp <- Created(id)
+      } yield resp
+  }
+
+  // get /jobs
+  private val getAllRoute: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root =>
+    jobs.all.flatMap(jobs => Ok(jobs))
+  }
+
+  val routes: HttpRoutes[F] = Router(
+    prefix -> (createJobRoute <+> getAllRoute)
+  )
+}
+
+object JobRoutes {
+  def resource[F[_]: Concurrent](jobs: Jobs[F]): Resource[F, JobRoutes[F]] =
+    Resource.pure(new JobRoutes[F](jobs))
 }
